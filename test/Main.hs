82caee13515e8d7851ed500bdd8f2200ff571c85
Denis Redozubov <denis.redozubov@gmail.com>
2018-03-14 19:44:53 +0300

    Missing file

---
+++ test/Main.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Control.Applicative
+import Data.ByteString as BS
+import SAX
+import Test.Hspec
+
+
+newtype World = World ByteString deriving (Show, Eq)
+data Hello = Hello { hHello :: ByteString, hWorld :: World, hIsDom :: Bool } deriving (Show, Eq)
+
+helloXml :: ByteString
+helloXml = "<?xml version=\"1.1\"?><f><foo><bar><hello><inner>Hello</inner><world> wor</world><world>ld!</world></hello></bar></foo></f>"
+
+helloParser :: SaxParser Hello
+helloParser = do
+  withTag' "foo" $ do
+    withTag' "hello" $ do
+      hello <- withTag "inner" text
+      world <- World . BS.concat <$> some (withTag "world" text)
+      isDom <- (withTag "is_dom" $ pure True) <|> pure False
+      pure $ Hello hello world isDom
+
+main :: IO ()
+main = hspec $ do
+  describe "parser" $ do
+    it "works" $ do
+      parseSax helloParser (streamXml helloXml) `shouldSatisfy`
+        \res -> case res of
+          (Done r ) -> r == Hello "Hello" (World " world!") False
+          _         -> False
